faa6886f213bb18aed91359895c5dc59ecff826a
Muhammad Aqeel <muhammadaqeel.sgd2@gmail.com>
2023-11-27 13:59:26 +0500

    Initial Commit

---
+++ ClientPortal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClientPortal
+//
+//  Created by Muhammad  Aqeel on 27/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
